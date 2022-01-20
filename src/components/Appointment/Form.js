7e6797c6dcba816b7f650ef144853c7866163040
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-20 12:51:13 -0500

    refactor Form component's save Button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -19,7 +19,7 @@ export default function Form(props){
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" >
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -45,7 +45,7 @@ export default function Form(props){
           >Cancel</Button>
           <Button 
           confirm 
-          onClick={() => props.onSave}
+          onClick={props.onSave}
           >Save</Button>
         </section>
       </section>
