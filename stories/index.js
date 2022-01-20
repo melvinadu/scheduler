35c03c60bdf9d0aab3853499f3fbb6d3e5277c75
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-20 01:19:29 -0500

    refactor InterviewerList component and respective files to use controlled list naming pattern

--- stories/index.js
+++ stories/index.js
@@ -123,12 +123,12 @@ storiesOf("Button", module)
         .add("Selected", () => (
           <InterviewerList
             interviewers={interviewers}
-            interviewer={3}
+            value={3}
           />
         ))
         .add("Clickable", () => (
           <InterviewerList
             interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+            onChange={action("setInterviewer")}
           />
         ));
