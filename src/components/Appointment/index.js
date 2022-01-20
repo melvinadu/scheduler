a4e3badb533a914b4f2dd0a4320c00ea146c35a8
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-20 16:57:09 -0500

    build Appointment component to display Header, Show, and Empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { Fragment } from 'react'
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+
 
 export default function Appointment(props){
   return (
     <article className="appointment">
+        <Header time={props.time}>
+        </Header>
 
+        {props.interview ? <Show student={props.interview.student} name={props.interview.interviewer.name} /> : <Empty/>}
+      
+    
     </article>
 
   );
 }
+
+
+
