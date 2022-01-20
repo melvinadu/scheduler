d724f1c5a97643fb004e663876cd5eb336262d6b
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-20 11:51:13 -0500

    build Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from 'react';
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props){
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,25 +25,27 @@ export default function Form(props){
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+          
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button 
           danger 
-
+          onClick={cancel}
+          
           >Cancel</Button>
           <Button 
           confirm 
-          
+          onClick={() => props.onSave}
           >Save</Button>
         </section>
       </section>
