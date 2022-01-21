d108b8f90d9c3b7ba7717b78be8c533241707465
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-21 04:14:18 -0500

    implement axios GET request to API for days data with React's useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
 import "components/Application.scss";
+
 import DayList from "./DayList";
 import "components/Appointment"
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -63,6 +49,18 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+
+    axios.get("http://localhost:8001/api/days")
+    .then((response) => {
+      console.log(response);
+      setDays(response.data);
+    })
+
+  },[])
+
 
   const appointmentArray = appointments.map(appointment => {
     return (
