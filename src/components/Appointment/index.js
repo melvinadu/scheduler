b99d5db05a046ae3a290d32e6055084f08ec84ac
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-22 14:05:28 -0500

    refactor Appointment component to make use of UseVisualMode custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,21 +3,41 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
 
 export default function Appointment(props){
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  // bring mode, transition and back properties of useVisualMode into component
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY //pass initial value and set initial state of mode variable in useVisualMode custom hook based on whether props.interview variable is true or false
+  );
+  
 
   return (
     <article className="appointment">
-        <Header time={props.time}>
-        </Header>
+        <Header time={props.time}/>
 
-        {/* SHOW component if interview is TRUE or EMPTY if interview is false */}
-        {props.interview ? 
-        <Show student={props.interview.student} name={props.interview.interviewer.name} /> 
-        :
-        <Empty/>}
-      
-    
+        { mode === EMPTY && 
+        <Empty 
+          onAdd={() => transition(CREATE)} // if button on Empty component is clicked, transition to condition where mode === CREATE
+        /> }
+
+        { mode === SHOW && 
+        <Show 
+          student={props.interview.student} 
+          name={props.interview.interviewer.name} 
+        /> }
+
+        { mode === CREATE && 
+        <Form 
+          interviewers={[]}
+          onCancel={() => back()} //if cancel button on Form component is clicked, call back function which transitions back to condition where mode === EMPTY
+        /> }
+        
     </article>
 
   );
