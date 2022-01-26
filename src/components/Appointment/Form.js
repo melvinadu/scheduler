80a355a07a3f38d1984bc6135800cd9c9d4b3cc1
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-26 10:38:30 -0500

    add more test for Form component and used test-driven development to implement validation feature on student name when creating a new appointment

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props){
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
@@ -15,6 +16,15 @@ export default function Form(props){
     reset();
     props.onCancel();
   };
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -31,7 +41,9 @@ export default function Form(props){
             onChange={(event) => setStudent(event.target.value)}
             data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
+
         <InterviewerList 
           interviewers={props.interviewers}
           value={interviewer}
@@ -48,7 +60,7 @@ export default function Form(props){
           >Cancel</Button>
           <Button 
           confirm 
-          onClick={() => {props.onSave(student, interviewer)}}
+          onClick={() => validate()}
           >Save</Button>
         </section>
       </section>
